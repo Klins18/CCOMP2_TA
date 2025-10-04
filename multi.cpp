fd5698329f675288c2fde9bba47af1364364e616
Klins18 <102160876+Klins18@users.noreply.github.com>
2025-10-04 09:52:47 -0500

    Create multi.cpp

---
+++ multi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/*void multi(int (*A)[3][3]){
+    
+}*/
+
+int main(){
+    
+    int arr[][3][3] = {{{1,2,3},{1,2,3},{1,2,3}},
+                       {{2,3,4},{2,3,4},{2,3,4}},
+                       {{0,0,0},{0,0,0},{0,0,0}}};
+    return 0;
+}
