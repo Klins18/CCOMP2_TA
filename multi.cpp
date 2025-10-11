7d70c9639987fddbab3aff66a9c31d1216dc49aa
Klins18 <102160876+Klins18@users.noreply.github.com>
2025-10-11 07:51:45 -0500

    Update multi.cpp

--- multi.cpp
+++ multi.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 using namespace std;
-/*void multi(int (*A)[3][3]){
+
+void print_mat(int (*A)[3][3]){
+  int *p = (int*)A;
+  for(int *i = p; i < p + 9; i += 3){
+    for(int *j = i; j < i + 3; j++){
+      cout << *j << " ";
+    }
+    cout << endl;
+  }
+}
+
+void multi(int (*A)[3][3]){
+    int fil = 3;
+    int col = 3;
+    int tam_cara = fil*col;
+    int *p = (int*)A;
+    int *q = p + tam_cara;
+    int *r = q + tam_cara;
     
-}*/
+    for(int *i = p; i < p + tam_cara; i++){
+        *(r + (i - p)) = (*i) * *(q + (i - p));
+    }
+}
 
 int main(){
     
     int arr[][3][3] = {{{1,2,3},{1,2,3},{1,2,3}},
                        {{2,3,4},{2,3,4},{2,3,4}},
                        {{0,0,0},{0,0,0},{0,0,0}}};
+    
+    multi(arr);                   
+    print_mat(arr+2);
+    
     return 0;
 }
