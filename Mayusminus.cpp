0ee11dd2457b32f9a56ab4a35d579198150c7608
Klins18 <102160876+Klins18@users.noreply.github.com>
2025-11-15 09:08:36 -0500

    functor

--- Mayusminus.cpp
+++ Mayusminus.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
-bool aMayuscula(){
-    return true;
-}
+class AMayuscula {
+public:
+    bool operator()() const {
+        return true;
+    }
+};
 
-bool aMinuscula(){ 
-    return false; 
-}
+class AMinuscula {
+public:
+    bool operator()() const {
+        return false;
+    }
+};
 
-
-void funCad(char* c, bool (*f)()) {
+template <typename Functor>
+void funCad(char* c, Functor f) {
     bool convertirMayus = f();
-
     for (int i = 0; c[i] != '\0'; i++) {
         if (convertirMayus) {
             if (c[i] >= 'a' && c[i] <= 'z') {
@@ -28,12 +33,12 @@ void funCad(char* c, bool (*f)()) {
 int main() {
     char cad1[] = "NamsnilK";
     char cad2[] = "Namsnilk";
-
-    funCad(cad1, aMayuscula);
+    
+    funCad(cad1, AMayuscula());
     std::cout << "Mayuscula: " << cad1 << "\n";
-
-    funCad(cad2, aMinuscula);
+    
+    funCad(cad2, AMinuscula());
     std::cout << "Minuscula: " << cad2 << "\n";
-
+    
     return 0;
 }
